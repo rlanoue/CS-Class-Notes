cd70e07b4c93324577f208aabe93da78d5e0fa6f
Jordan Lanoue <rlanoue@ou.edu>
2019-02-07 16:05:38 -0600

    Added CS Notes 2/7/19

---
+++ CS Notes.java
@@ -0,0 +1,6 @@
+Sharing Data Between Classes
+Another way to share data is inheritence
+-- Extended and Protected
+
+Protected replaces get/set and is then able to be accessed inside the class and classes 
+extended from the original class
